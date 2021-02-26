c36087020af3a049b414f13ecd3e3f3afc90f4c2
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2021-02-26 21:38:53 +0800

    Create rails with no test using Postgres

---
+++ config/webpack/environment.js
@@ -0,0 +1,9 @@
+const { environment } = require('@rails/webpacker')
+
+environment.plugins.append(‘Provide’, new webpack.ProvidePlugin({
+  $: ‘jquery’,
+  jQuery: ‘jquery’,
+  Popper: [‘popper.js’, ‘default’]
+}));
+
+module.exports = environment
